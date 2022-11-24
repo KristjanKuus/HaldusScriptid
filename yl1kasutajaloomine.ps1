185b25f509e4e14dc688801c0be4f05743886b9e
KristjanKuus <74591012+KristjanKuus@users.noreply.github.com>
2022-11-24 19:01:18 +0200

    Add files via upload
    Esimene ülesanne - lokaalse kasutaja loomine tema ees- ja perenimi järgi

---
+++ yl1kasutajaloomine.ps1
@@ -0,0 +1,7 @@
+﻿$eesnimi = Read-Host "Palun sisestage oma eesnimi"
+$perenimi = Read-Host "Palun sisestage oma perenimi"
+$parool = "Parool1!" | ConvertTo-SecureString -AsPlainText -Force
+$taisnimi = "$eesnimi.$perenimi"
+$kirjeldus = "$eesnimi $perenimi windowsi konto"
+
+New-LocalUser -Name $taisnimi -Password $parool -FullName $taisnimi -Description $kirjeldus
