e42a3396bb209f1261129c25007e271d549d3417
KristjanKuus <74591012+KristjanKuus@users.noreply.github.com>
2023-01-09 14:30:41 +0200

    Update yl3kasutajaloomine.ps1

--- yl3kasutajaloomine.ps1
+++ yl3kasutajaloomine.ps1
@@ -1,14 +1,40 @@
-$eesnimi = Read-Host "Palun sisestage oma eesnimi"
-$perenimi = Read-Host "Palun sisestage oma perenimi"
-$parool = "Parool1!" | ConvertTo-SecureString -AsPlainText -Force
+
+function Translit {
+    param(
+    	[string] $inputstring
+     	 )
+  	$Translit = @{
+     	[char]'ä' = "a"
+     	[char]'õ' = "o"
+     	[char]'ü' = "u"
+     	[char]'ö' = "o"
+     	}
+     	$outputstring=""
+     	foreach ($character in $inputCharacter = $inputstring.ToCharArray())
+    	{
+     	if ($Translit[$character] -cne $Null){
+     	$outputString += $Translit[$character]
+     	} else {
+        	$outputString += $character
+    	}
+     	 }
+        Write-Output $outputstring
+        }
+
+$eesnimi = Read-Host "Palun sisestage kasutaja eesnimi"
+$perenimi = Read-Host "Palun sisestage kasutaja perenimi"
+$parool = Read-Host "Palun sisestage kasutaja parool" | ConvertTo-SecureString -AsPlainText -Force
 $taisnimi = "$eesnimi.$perenimi"
+$taisnimi = Translit($taisnimi)
+$adnimi = $taisnimi + "@sv-kool.local"
 $kirjeldus = "$eesnimi $perenimi windowsi konto"
 
-
-net user $taisnimi
-if ($? -eq $true) { 
-    Write-host "Selline $taisnimi kasutaja on juba olemas - ei saa seda lisada" 
-     }
-else {
-    New-LocalUser -Name $taisnimi -Password $parool -FullName $taisnimi -Description $kirjeldus
+if (Get-ADUser -F {SamAccountName -eq $taisnimi}) 
+    { 
+    Write-host "Kasutaja nimega $taisnimi on juba olemas - ei saa seda lisada" 
+    }
+    else 
+    {
+    New-ADUser -Name $taisnimi -DisplayName $taisnimi -GivenName $taisnimi -Title $taisnimi -UserPrincipalName $adnimi -AccountPassword (ConvertTo-SecureString $parool -AsPlainText -force) -Enabled $true 
+    Write-host "Kasutaja $taisnimi edukalt lisatud systeemi"
     }
