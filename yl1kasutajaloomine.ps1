b15985dd2c364d3cd09dc8aaaf382ca0c0f70500
KristjanKuus <74591012+KristjanKuus@users.noreply.github.com>
2023-01-16 08:43:28 +0200

    Update yl1kasutajaloomine.ps1

--- yl1kasutajaloomine.ps1
+++ yl1kasutajaloomine.ps1
@@ -1,7 +1,11 @@
 ﻿$eesnimi = Read-Host "Palun sisestage oma eesnimi"
 $perenimi = Read-Host "Palun sisestage oma perenimi"
+#$eesnimi ja $perenimi küsivad kasutajalt oma eesnime ja perenime, kasutaja vastused salvestatakse konto ees ja perenimena
 $parool = "Parool1!" | ConvertTo-SecureString -AsPlainText -Force
+#Parool1! on läbi selle skripti tehtud konto parool
 $taisnimi = "$eesnimi.$perenimi"
+#eesnimi ja perenimi kombineeritud ühte, et teha kontole nimi
 $kirjeldus = "$eesnimi $perenimi windowsi konto"
+#Uue kasutaja lühikirjeldus
 
 New-LocalUser -Name $taisnimi -Password $parool -FullName $taisnimi -Description $kirjeldus
